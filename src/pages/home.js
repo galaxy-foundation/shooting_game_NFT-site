0afeef54bc196ac6f511b0018f42c65aebb7b3c1
BigBen3918 <benjamin199551@gmail.com>
2021-10-25 08:06:37 +0300

    add: login

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,30 @@
 import React from "react";
 import Nav from "../components/layout/nav";
+import LoginDialog from "../components/home/logindialog";
 // import topImg from '../assets/img/homeTopImg.png';
 // import About from '../components/home/about';
 // import PetToken from '../components/home/petToken';
-import BottomSection from '../components/home/bottom';
+import BottomSection from "../components/home/bottom";
 import CharactSlide from "../components/home/character-slide";
 import GameIntro from "../components/home/gameintro";
-import defiVideo from "../assets/video/pet_video.mp4";
+import defiVideo from "../assets/video/video.mp4";
 import splitImg from "../assets/img/shooting/split_symbol.png";
 // import {Grid} from '@material-ui/core';
 // import { Link } from "react-router-dom";
 
 function Home() {
+  const [open, setOpen] = React.useState(false);
+  const [loginState, setLoginState] = React.useState(false);
+  const [tabValue, setTabValue] = React.useState("0");
+
   return (
     <div>
       <div className="x-home-top">
-        <Nav />
+        <Nav
+          setOpen={setOpen}
+          loginState={loginState}
+          setTabValue={setTabValue}
+        />
         <div className="x-home-top-video" style={{ zIndex: "-1" }}>
           <video autoPlay loop muted width="100%" height="90%">
             <source src={defiVideo} type="video/mp4" />
@@ -27,7 +36,7 @@ function Home() {
                 We must save our world <br />
                 with our power
               </div>
-              <button className="x-greenImgBtn">Play (soon)</button>
+              <button className="x-greenImgBtn">PLAY NOW</button>
             </div>
           </div>
         </div>
@@ -46,6 +55,13 @@ function Home() {
         {/* <PetToken /> */}
         <BottomSection />
       </div>
+      <LoginDialog
+        open={open}
+        setOpen={setOpen}
+        setLoginState={setLoginState}
+        tabValue={tabValue}
+        setTabValue={setTabValue}
+      />
     </div>
   );
 }
